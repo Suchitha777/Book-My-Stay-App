acd79b36318071f8b0e1f2ebf037a50b4128760b
Suchitha777 <suchithakundur@gmail.com>
2026-03-22 20:27:57 +0530

    Base application setup

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        printWelcomeMessage();
+    }
+
+    public static void printWelcomeMessage() {
+        System.out.println("===================================");
+        System.out.println("     Welcome to Book My Stay App   ");
+        System.out.println("===================================");
+    }
+}
