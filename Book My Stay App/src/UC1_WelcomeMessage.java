22197af2a481989cae13616c5568ffc0fdfc48c9
Suchitha777 <suchithakundur@gmail.com>
2026-03-22 21:57:45 +0530

    Cleaned project structure

--- Book My Stay App/src/UC1_WelcomeMessage.java
+++
@@ -1,8 +0,0 @@
-public class UC1_WelcomeMessage {
-
-    public static void execute() {
-        System.out.println("=================================");
-        System.out.println(" Welcome to Book My Stay System ");
-        System.out.println("=================================");
-    }
-}
